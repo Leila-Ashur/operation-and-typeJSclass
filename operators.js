d96194b36d48d21323ae8faa8a038b1fd687842c
leilaashur <leilahashur@gmail.com>
2023-06-23 09:57:04 +0300

    operators commit

---
+++ operators.js
@@ -0,0 +1,52 @@
+let a = 30;
+console.log(a);
+
+// Arthmetic operators
+
+const num =10;
+const num2 = 30;
+// addition +
+console .log (num + num2);
+console.log(num - num2);
+console.log((num / num2));
+console. log(num * num2);
+console.log(num % num2);
+
+let age =39;
+let newAge = age++
+
+
+console.log(age);
+age--;
+console.log("age",age);
+
+age += 5;
+console.log("new Age", age)
+
+let powerNum = num ** num2;
+console.log(powerNum)
+
+var b = 10
+var c = "10"
+let comparison = b == c;
+console.log("comparison",comparison)
+let notEqual = b !=c;
+console.log ("Not Equal", notEqual);
+let strictly =b ===c;
+console.log("strictly",strictly)
+let  strictlyNot= b !== c;
+console.log({strictlyNot})
+
+//Logical Operators
+
+const age1 =25;
+const age2 =38;
+
+if(age1 > age2 && num > num2){
+    console.log(true)
+
+}
+else{
+    console.log(false)
+}
+
